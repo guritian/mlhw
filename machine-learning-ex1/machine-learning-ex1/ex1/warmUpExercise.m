a79b2b749d120a96f9570cb6e514b6ac82da6b24
gh <1179681903@qq.com>
2020-07-11 00:37:28 +0800

    完成了warmUpExercise  并进行matlab版本管理测试

--- machine-learning-ex1/machine-learning-ex1/ex1/warmUpExercise.m
+++ machine-learning-ex1/machine-learning-ex1/ex1/warmUpExercise.m
@@ -4,12 +4,14 @@ function A = warmUpExercise()
 
 A = [];
 % ============= YOUR CODE HERE ==============
-% Instructions: Return the 5x5 identity matrix 
+% Instructions: Return the 5x5 identity matrix   返回一个 5X5的 单位矩阵
 %               In octave, we return values by defining which variables
 %               represent the return values (at the top of the file)
 %               and then set them accordingly. 
 
 
+%I = eye(n) 返回一个主对角线元素为 1 且其他位置元素为 0 的 n×n 单位矩阵。
+A = eye(5);
 
 
 
